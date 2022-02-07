cf45952d851f0124cb64b8259c3fbb0f344e7ae4
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-02-07 20:49:33 +0000

    Handling exceptions and clear screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,14 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    // método para limpar o console
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen(){
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+
+
 
     // metodo para ler a posição do usuário
     public static ChessPosition readChessPosition(Scanner sc){
